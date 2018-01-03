851aa38bbd23ae2a8415ca98b0620c16f956950e
michelle moyer <moyer.mj@gmail.com>
2018-01-02 18:32:30 -1000

    complete vault

--- vault.js
+++ vault.js
@@ -1,4 +1,19 @@
 'use strict';
 module.exports = function() {
-  
+    let vault = {};
+    let setValue = function (key, value){
+      vault[key] = value;
+    };
+
+    let getValue = function (key){
+      if (key === undefined || !vault[key] ){
+        return null;
+      } else {
+        return vault[key];
+      };
+    };
+    return {
+      setValue: setValue,
+      getValue: getValue,
+    };
 };
